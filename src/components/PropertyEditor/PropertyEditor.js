9713958aaa13531ce8d6c078f63ac01c59287cad
dan koenig <dangerbird@dans-MacBook-Pro.local>
2024-11-20 21:51:27 -0500

    add editor max

--- src/components/PropertyEditor/PropertyEditor.js
+++ src/components/PropertyEditor/PropertyEditor.js
@@ -1,6 +1,5 @@
 
-import { Fragment } from 'react';
-import { UnitEditor, ColorEditor, EnumEditor } from '../Editors';
+import EditorShell from '../EditorShell/EditorShell';
 const PropertyEditor = (props) => {
     const valueUpdateHandler = (name, value) => {
         props.onPropertyUpdated(name, value);
@@ -12,25 +11,13 @@ const PropertyEditor = (props) => {
                 {props.selectedProperties.filter(item => item.selected).map((item, i) => (
                     <li key={i}>
                         {item.name}
-                        <ul>
-                            {item.editors.map((editor) => (
-                                <Fragment key={i}>
-                                    {editor.type === 'unit' && (
-                                        <li>
-                                            <UnitEditor
-                                                onValueUpdate={(value) => valueUpdateHandler(item.name, value)}
-                                                subType={editor.subType} 
-                                            />
-                                        </li>
-                                    )}
-                                    {editor.type === 'color' && <li><ColorEditor /></li>}
-                                </Fragment>
-                            ))}
-                            <li><EnumEditor
-                                onValueUpdate={(value) => valueUpdateHandler(item.name, value)}
-                                subType={item.editors?.find(editor => editor.type === 'enum')?.subType}
-                            /></li>
-                        </ul>
+                        <div>
+                            <EditorShell 
+                                editors={item.editors}
+                                onPropertyUpdated={(value) => {valueUpdateHandler(item.name, value)}}
+                                max={item.max}
+                            />
+                        </div>
                     </li>
                 ))}
             </ul>
