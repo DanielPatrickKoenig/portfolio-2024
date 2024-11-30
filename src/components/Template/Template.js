74b015921f7b2aa03b72da6d07b76bbd903f4dd1
dan koenig <dangerbird@dans-MacBook-Pro.local>
2024-11-29 19:41:04 -0500

    make tools react to templates and selectors

--- src/components/Template/Template.js
+++ src/components/Template/Template.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const Template = (props) => {
     const [templateContent, setTemplateContent] = useState(props.templates[props.templateIndex >= 0 ? props.templateIndex : 2].content);
@@ -7,8 +7,24 @@ const Template = (props) => {
         setTemplateContent(e.target.value);
     }
 
+    const templateSelected = (e) => {
+        const selectedTemplate = props.templates[e.target.value];
+        setTemplateContent(selectedTemplate.content);
+        props.onTemplateSelected(selectedTemplate);
+    }
+
     return (
         <div>
+            <select onChange={templateSelected}>
+                {props.templates.map((item, i) => (
+                    <option
+                        value={i}
+                        key={i}
+                    >
+                        {item.name}
+                    </option>
+                ))}
+            </select>
             <textarea
                 value={templateContent}
                 onChange={changeContent}
