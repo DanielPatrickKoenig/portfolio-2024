0d8e2b0afa4e7a059bfbb93e24ac6f5a6d1cc2b5
dan koenig <dangerbird@dans-MacBook-Pro.local>
2024-12-30 16:15:12 -0500

    add swiping

--- src/components/PortfolioItemNav/PortfolioItemNav.js
+++ src/components/PortfolioItemNav/PortfolioItemNav.js
@@ -1,19 +1,32 @@
 import { useState } from 'react';
+import { processEvent } from "../../utils/general";
 import './PortfolioItemNav.css';
 const PortfolioItemNav = (props) => {
     const [dragging, setDragging] = useState(false)
     const [pointer, setPointer] = useState({});
-    const [movement, setMovement] = useState({});
+    const [movement, setMovement] = useState({ x: 0, y: 0 });
+    const switMin = 50;
     const swipeStart = (e) => {
         setDragging(true);
+        const position = processEvent(e);
+        setPointer(position);
     }
     const swipeMove = (e) => {
         if(dragging) {
-            
+            const { x, y } = processEvent(e);
+            const position = { x: pointer.x - x, y: pointer.y - y };
+            setMovement(position);
         }
     }
-    const swipeEnd = (e) => {
+    const swipeEnd = () => {
         setDragging(false);
+        if (movement.x > switMin){
+            props.onMoveRight();
+        }
+        if (movement.x < switMin * -1){
+            props.onMoveLeft();
+        }
+        setMovement({ x: 0, y: 0 });
     }
     return (
         <div
---
+++ src/utils/general.js
@@ -0,0 +1,19 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function processEvent (e) {
+    if (e.touches) {
+        e.preventDefault();
+        return { x: e.touches[0].clientX, y: e.touches[0].clientY };
+    }
+    return { x: e.clientX, y: e.clientY };
+}
+
+export { getParameterByName, processEvent };
