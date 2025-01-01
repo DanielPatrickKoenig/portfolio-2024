a7d3099afddae3f73d860327587b1ec092b7a55d
dan koenig <dangerbird@dans-MacBook-Pro.local>
2025-01-01 13:46:45 -0500

    refine style

--- src/components/PortfolioItem/PortfolioItem.js
+++ src/components/PortfolioItem/PortfolioItem.js
@@ -7,13 +7,13 @@ const PortfolioItem = (props) => {
         const x = Math.round(jstrig.orbit(0, 300, rotation, 'cos'));
         const z =  Math.round(jstrig.orbit(0, -100, rotation, 'sin'));
         const inCenter = rotation % 360 === 0 || rotation === 0;
-        const xRot = inCenter ? -30 : -30;
+        const xRot = -25;
         const xRotation = z < 0 ? xRot * -1 : xRot;
         const zRot = x * .125;
         const zRotation = z < 0 ? zRot * -.8 : zRot;
         const scaleFactor = z < 0 ? .5 : 1 - (Math.abs(x) / 600);
         const transform = `translateX(${x}px) translateY(${z}px) rotateX(${xRotation}deg) rotateY(${rotation}deg) rotateZ(${zRotation}deg) scale(${scaleFactor})`;
-        const transformbottom = `translateX(${x}px) translateY(${z}px) rotateX(0deg) rotateY(${rotation}deg) rotateZ(0deg) scale(${scaleFactor})`;
+        const transformbottom = `translateX(${x}px) translateY(${z}px) rotateX(0deg) rotateY(${rotation}deg) rotateZ(0deg) scale(${scaleFactor - (z < 90 && z > 0 ? .1 : 0)})`;
         const opacity = 1 - Math.abs(x * .0034) - (z < 0 ? .25 : 0);
         const filter = z < 0 ? 'blur(6px)' : 'unset';
         const zIndex = Math.round(z);
